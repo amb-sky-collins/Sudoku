55478aa1bf9fdfb776407434b7deda2dee3991a8
AngelicaBarrett <116841969+AngelicaBarrett@users.noreply.github.com>
2022-11-29 18:30:26 -0500

    moved Cell class into sudoku_generator.py and created Board class in sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -209,3 +209,60 @@ def generate_sudoku(size, removed):
     sudoku.remove_cells()
     board = sudoku.get_board()
     return board
+
+
+# recommended class
+class Cell:
+
+    def __init__(self, value, row, col, screen):
+        pass
+
+    def set_cell_value(self, value):
+        pass
+
+    def set_sketched_value(self, value):
+        pass
+
+    def draw(self):
+        pass
+
+
+# recommended class
+class Board:
+
+    def __init__(self, width, height, screen, difficulty):
+        pass
+
+    def draw(self):
+        pass
+
+    def select(self, row, col):
+        pass
+
+    def click(self, x, y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
+
